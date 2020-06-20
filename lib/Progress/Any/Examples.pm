cac4eb65ea6c0916e01498dfc5a00ac95add5d63
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-20 19:50:16 +0700

    Initial commit

---
+++ lib/Progress/Any/Examples.pm
@@ -0,0 +1,29 @@
+package Progress::Any::Examples;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Examples for Progress::Any
+
+=head1 SYNOPSIS
+
+See scripts included in this distribution.
+
+=head1 DESCRIPTION
+
+This distribution include the following scripts to demonstrate how to use
+L<Progres::Any>:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Progress::Any>
+
+C<Progress::Any::Output::*>
+
+=cut
